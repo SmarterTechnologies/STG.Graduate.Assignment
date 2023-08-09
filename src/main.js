c6ef67572406339c535fc5a45a92d6be803c9139
Joe Frampton <framptonjf@cardiff.ac.uk>
2023-08-09 17:52:44 +0100

    API is functioning and data is loading. CSS still needed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
 console.log("main.js");
+
+let APIKEY = '9d33630a5cce4194a6b140510230808'
+
+call = 'http://api.weatherapi.com/v1/current.json?key=9d33630a5cce4194a6b140510230808&q=Newport&aqi=no'
+
+function sendAPIRequest(location){
+    fetch("http://api.weatherapi.com/v1/current.json?key=" + APIKEY + "&q=" + location + "&aqi=no")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        showWeather(data)
+        console.log(data)
+    })
+}
+
+function showWeather (data) {
+    let location = data.location.name
+    let description = data.current.condition.text
+    let icon = data.current.condition.icon
+    let temperature = data.current.temp_c
+    let windSpeed = data.current.wind_mph
+    let windDirection = data.current.wind_degree
+
+    document.querySelector(".location").innerText = location
+    document.querySelector(".description").innerText = description
+    document.querySelector(".temperature").innerText = temperature + "°C"
+    document.querySelector(".windSpeed").innerText = windSpeed + " mph"
+    document.getElementById("weatherIcon").src = icon
+    document.getElementById("weatherIcon").alt += description + " weather"
+
+
+    console.log(location, description, icon, temperature, windSpeed, windDirection)
+}
+
+function searchByLocation(){
+    let location = document.getElementById("locationSearch").value
+    if (location !== ''){
+        sendAPIRequest(document.getElementById("locationSearch").value)
+    } else {
+        document.getElementById("locationSearch").placeholder = "Please enter a location"
+    }
+
+}
